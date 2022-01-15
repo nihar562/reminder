4beb61840fe37edefa3a3c76df429ac85c569ebd
Nihar Ranjan Sahoo <85186279+nihar562@users.noreply.github.com>
2022-01-15 23:13:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-D = int(input())
-N = int(input())
-wr = D*(90/100)
-disk_used = 0
-n =0
-while(n < N):
-    du = int(input())
-    disk_used = disk_used+du
-    if disk_used >= wr:
-        print('Warning')
-        break
-    n = n+1
-if disk_used < wr:   
-    print('safe')
+def Reminder(D,N):
+    wr = D*(90/100)
+    disk_used = 0
+    n =0
+    while(n < N):
+        du = int(input())
+        disk_used = disk_used+du
+        if disk_used >= wr:
+            return 'Warning'
+        n = n+1
+    if disk_used < wr:   
+        return 'safe'
+DiskSize = int(input())
+No_of_days= int(input())
+print(Reminder(DiskSize,No_of_days))
